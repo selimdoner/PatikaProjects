4a3aa5fc209295ad4606e885edb622aa23cf474e
Selim Doner <s.selimdoner@gmail.com>
2022-09-06 17:48:03 +0300

    11th

---
+++ src/main/java/Project03.java
@@ -0,0 +1,20 @@
+public class Project03 {
+
+    /*
+
+    Project 3
+    [7, 5, 1, 8, 3, 6, 0, 9, 4, 2] dizisinin Binary-Search-Tree aşamalarını yazınız.
+
+    Örnek: root x'dir. root'un sağından y bulunur. Solunda z bulunur vb.
+
+    =================================================================================
+
+                     6==>Root
+                4        8
+              2 - 5    7 - 9
+            1 - 3
+              0
+    rootun solunda 6'dan küçük rakamlar, rootun sağında 6'dan büyük rakamlar bulunur.
+
+     */
+}
