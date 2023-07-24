9aaf922fc4fd00f6f3a541b4d854a58146a24537
SelimDoner01 <121343874+SelimDoner01@users.noreply.github.com>
2023-07-24 13:21:00 +0300

    12th

---
+++ src/main/java/QuickSort.java
@@ -0,0 +1,45 @@
+public class QuickSort {
+    public void quickSort(int[] array) {
+        if (array == null || array.length == 0) {
+            return;
+        }
+        quickSort(array, 0, array.length - 1);
+    }
+
+    private void quickSort(int[] array, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(array, low, high);
+            quickSort(array, low, pivotIndex - 1);
+            quickSort(array, pivotIndex + 1, high);
+        }
+    }
+
+    private int partition(int[] array, int low, int high) {
+        int pivot = array[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (array[j] <= pivot) {
+                i++;
+                swap(array, i, j);
+            }
+        }
+        swap(array, i + 1, high);
+        return i + 1;
+    }
+
+    private void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {2, 17, -4, 42, 9, 26, 11, -1, -28, 5};
+        QuickSort quickSort = new QuickSort();
+        quickSort.quickSort(array);
+        System.out.println("Sorted array: ");
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+    }
+}
